c541fccd5644f0904cfce26ec03722a9adb1f59e
NicolasSKing <nocoben10@gmail.com>
2026-03-09 16:30:10 +0800

    finished

---
+++ one_number_subtracting_for_the_rest_of_the_numbers.py
@@ -0,0 +1,18 @@
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+third_number = float(input("Enter the third number: "))
+fourth_number = float(input("Enter the fourth number: "))
+fifth_number = float(input("Enter the fifth number: "))
+sixth_number = float(input("Enter the sixth number: "))
+seventh_number = float(input("Enter the seventh number: "))
+eighth_number = float(input("Enter the eighth number: "))
+nineth_number = float(input("Enter the nineth number: "))
+tenth_number = float(input("Enter the tenth number: "))
+
+sum = second_number + third_number + fourth_number + fifth_number + sixth_number + seventh_number + eighth_number + nineth_number + tenth_number
+print(first_number - sum)
+
+
+
+
+
