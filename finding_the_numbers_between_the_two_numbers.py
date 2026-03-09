eaadcd81c78d9bd15f5041f1e114f156756a8ee9
NicolasSKing <nocoben10@gmail.com>
2026-03-09 17:27:49 +0800

    end_of_batch_2

---
+++ finding_the_numbers_between_the_two_numbers.py
@@ -0,0 +1,11 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+numbers_between_two_numbers = []
+for i in range(first_number + 1, second_number, 1):
+    numbers_between_two_numbers.append(i)
+print(numbers_between_two_numbers)
+
+
+
+
+
