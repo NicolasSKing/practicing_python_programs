60e39e7f975932e4dedd99f152b6cdb4b0207ba2
NicolasSKing <nocoben10@gmail.com>
2026-03-09 16:45:14 +0800

    done

---
+++ finding_how_many_are_the_even_numbers.py
@@ -0,0 +1,9 @@
+even_numbers = 0
+for i in range(10):
+    numbers = float(input("Enter a number: "))
+    if numbers % 2 == 0:
+        even_numbers += 1
+print(f"There are {even_numbers} even numbers")
+
+
+
