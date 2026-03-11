c884bdce9279c8fbcdf5c0489f1aba50cbf947b3
NicolasSKing <nocoben10@gmail.com>
2026-03-11 10:43:18 +0800

    done_first_3rd_batch

---
+++ finding_numbers_that_is_not_duplicate.py
@@ -0,0 +1,15 @@
+my_numbers = []
+
+for i in range(10):
+    numbers = float(input("Enter a number: "))
+    my_numbers.append(numbers)
+
+no_duplicates = set(my_numbers)
+print(no_duplicates)
+
+
+
+
+
+
+
