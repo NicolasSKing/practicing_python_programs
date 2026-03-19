9d0b5c53eee25e7ba990ede4751375f11275d27e
NicolasSKing <nocoben10@gmail.com>
2026-03-19 14:14:27 +0800

    changing_the_statement_of_both_the_inputs

--- finding_the_numbers_between_the_two_numbers.py
+++ finding_the_numbers_between_the_two_numbers.py
@@ -1,5 +1,5 @@
-first_number = int(input("Enter the first number: "))
-second_number = int(input("Enter the second number: "))
+first_number = int(input("Please enter the first number: "))
+second_number = int(input("Please enter the second number: "))
 numbers_between_two_numbers = []
 for i in range(first_number + 1, second_number, 1):
     numbers_between_two_numbers.append(i)
