5f1ad67ffcb7bbb2942149a4ae4c9b3eeff21e3d
NicolasSKing <nocoben10@gmail.com>
2026-03-19 14:03:15 +0800

    changing_the_variable_name

--- finding_numbers_not_ending_at_zero.py
+++ finding_numbers_not_ending_at_zero.py
@@ -1,7 +1,7 @@
-numbers_that_dont_have_zero = []
+all_the_numbers_that_dont_have_zero = []
 for i in range(101):
     if i % 10 != 0:
-        numbers_that_dont_have_zero.append(i)
-print(numbers_that_dont_have_zero)
+        all_the_numbers_that_dont_have_zero.append(i)
+print(all_the_numbers_that_dont_have_zero)
 
 
