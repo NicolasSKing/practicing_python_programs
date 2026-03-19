1507fb624875d9f799ba805267c8c43ebb14840d
NicolasSKing <nocoben10@gmail.com>
2026-03-19 14:30:40 +0800

    changed_the_variable_name

--- printing_all_the_even_numbers_using_for_loop.py
+++ printing_all_the_even_numbers_using_for_loop.py
@@ -1,6 +1,6 @@
-even_numbers = []
+all_the_even_numbers = []
 for i in range(101):
     if i % 2 == 0:
-        even_numbers.append(i)
-print(even_numbers)
+        all_the_even_numbers.append(i)
+print(all_the_even_numbers)
 
