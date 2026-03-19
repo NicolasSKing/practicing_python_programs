27b0023178728a6712f4503f0bd9015db6560e1f
NicolasSKing <nocoben10@gmail.com>
2026-03-19 13:58:16 +0800

    adding_please_on_the_input_statement

--- finding_how_many_the_odd_numbers.py
+++ finding_how_many_the_odd_numbers.py
@@ -1,6 +1,6 @@
 odd_numbers = 0
 for i in range(10):
-    numbers = float(input("Enter a number: "))
+    numbers = float(input("Please enter a number: "))
     if numbers % 2 != 0:
         odd_numbers += 1
 print(f"There are {odd_numbers} odd numbers")
