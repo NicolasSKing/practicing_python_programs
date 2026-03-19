a83c033bb589dfd1d66f1fa62b065f8dd8bb8bef
NicolasSKing <nocoben10@gmail.com>
2026-03-19 13:36:57 +0800

    replaced_the_enter_statement_with_type

--- computing_for_the_quotient_of_two_numbers_with_decimals.py
+++ computing_for_the_quotient_of_two_numbers_with_decimals.py
@@ -1,3 +1,3 @@
-first_number = float(input("Enter the first number: "))
-second_number = float(input("Enter the second number: "))
+first_number = float(input("Type the first number: "))
+second_number = float(input("Type the second number: "))
 print(first_number / second_number)
