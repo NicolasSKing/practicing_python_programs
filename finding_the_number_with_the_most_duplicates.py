3d7f667868254cfc9e06c0d9011075d2a9ca2d2e
NicolasSKing <nocoben10@gmail.com>
2026-03-19 16:10:33 +0800

    finished_making_the_whole_foor_loop_or_the_whole_logic_of_the_code

--- finding_the_number_with_the_most_duplicates.py
+++ finding_the_number_with_the_most_duplicates.py
@@ -4,4 +4,11 @@ while True:
     if not user_input.isdigit():
         break
     numbers.append(int(user_input))
+most = numbers[0]
+max_count = numbers.count(most)
+for num in numbers:
+    if numbers.count(num) > max_count:
+        most = num
+        max_count = numbers.count(num)
+print("Number with most duplicates:", most)
 
