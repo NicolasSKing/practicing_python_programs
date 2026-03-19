84afb20f3561a485f3e7f9f88449341e19c08c2f
NicolasSKing <nocoben10@gmail.com>
2026-03-19 13:24:58 +0800

    changed_the_input_statement

--- changing_your_name_to_pascal_case.py
+++ changing_your_name_to_pascal_case.py
@@ -1,2 +1,2 @@
-full_name = input("Enter your full name in incorrect casing: ")
+full_name = input("Please enter your full name in incorrect casing: ")
 print("".join(word.capitalize() for word in full_name.split()))
