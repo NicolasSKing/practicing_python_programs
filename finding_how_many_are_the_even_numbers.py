e8f78848d28af119d492ef96422039d049957cb2
NicolasSKing <nocoben10@gmail.com>
2026-03-19 13:56:09 +0800

    changing_the_input_statement

--- finding_how_many_are_the_even_numbers.py
+++ finding_how_many_are_the_even_numbers.py
@@ -1,6 +1,6 @@
 even_numbers = 0
 for i in range(10):
-    numbers = float(input("Enter a number: "))
+    numbers = float(input("Please enter a number: "))
     if numbers % 2 == 0:
         even_numbers += 1
 print(f"There are {even_numbers} even numbers")
