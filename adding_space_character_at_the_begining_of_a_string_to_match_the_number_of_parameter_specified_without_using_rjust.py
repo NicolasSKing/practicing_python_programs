e383a31aed84ea834f4e7a8928fc562138983f1a
NicolasSKing <nocoben10@gmail.com>
2026-03-20 14:18:40 +0800

    replaced_the_0_in_the_character_to_add_in_the_begining_with_space

--- adding_space_character_at_the_begining_of_a_string_to_match_the_number_of_parameter_specified_without_using_rjust.py
+++ adding_space_character_at_the_begining_of_a_string_to_match_the_number_of_parameter_specified_without_using_rjust.py
@@ -1,7 +1,7 @@
 text = input("Enter a text: ")
 width = int(input("Enter a width: "))
 if len(text) < width:
-    result = (width - len(text)) * "0" + text
+    result = (width - len(text)) * " " + text
 else:
     result = text
 print(result)
