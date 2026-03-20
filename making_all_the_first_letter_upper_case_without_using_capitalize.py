9bbb950e7ca8f87149a4e85ff140a8d46bdeb9cf
NicolasSKing <nocoben10@gmail.com>
2026-03-20 11:14:57 +0800

    made_all_the_logic_using_if_for_and_else_then_printing_the_result

--- making_all_the_first_letter_upper_case_without_using_capitalize.py
+++ making_all_the_first_letter_upper_case_without_using_capitalize.py
@@ -2,3 +2,15 @@ text = input("Enter text: ")
 lower = "abcdefghijklmnopqrstuvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 result = ""
+if text:
+    first_char = text[0]
+    if first_char in lower:
+        result += upper[lower.index(first_char)]
+    else:
+        result += first_char
+    for ch in text[1:]:
+        if ch in upper:
+            result += lower[upper.index(ch)]
+        else:
+            result += ch
+print("Capitalized:", result)
