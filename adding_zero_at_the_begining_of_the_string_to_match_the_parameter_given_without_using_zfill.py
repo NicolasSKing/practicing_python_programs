72d899977212add462bc8580d08c0b7b419ada8e
NicolasSKing <nocoben10@gmail.com>
2026-03-20 14:26:09 +0800

    made_the_variables_and_logics_using_if_and_else_then_adding_the_print_statements

---
+++ adding_zero_at_the_begining_of_the_string_to_match_the_parameter_given_without_using_zfill.py
@@ -0,0 +1,10 @@
+text = input("Enter a text: ")
+width = int(input("Enter a width: "))
+if len(text) < width:
+    result = (width - len(text)) * "0" + text
+else:
+    result = text
+print(result)
+
+
+
